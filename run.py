6e56c482a4bf276d4ff0a5b67bf8aa0ff2c6a650
Mamebousso007 <mdsyllaisidk@groupeisi.com>
2024-10-14 21:06:30 +0100

    Corection 10

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app, db
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Utiliser le port spécifié par Render ou 5000 par défaut
+    app.run(host='0.0.0.0', port=port)  # Exécutez l'application sur toutes les interfaces réseau
