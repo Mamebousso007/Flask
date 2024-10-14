4eb09c0c7a6dca1051c5d09bbdd830996cae7719
Mamebousso007 <mdsyllaisidk@groupeisi.com>
2024-10-14 22:17:31 +0100

    update config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     FLASK_ENV = os.getenv("FLASK_ENV")
     
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "postgresql://users:4oFabgHlc67eGxeqIqbSEK5y64qVv3jK@dpg-cs6illd6l47c73ffpfcg-a/flask_db_n4qv")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SECRET_KEY = os.getenv('SECRET_KEY') 
